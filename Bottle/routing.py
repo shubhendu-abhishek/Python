09e4bb24b27ff3bb1807766c03794f8154c9cca3
Shubhendu Abhishek <shubhendu.abhishek@above-inc.com>
2018-01-31 22:04:45 +0530

    routing update with custom error

--- Bottle/routing.py
+++ Bottle/routing.py
@@ -1,4 +1,4 @@
-from bottle import route, run
+from bottle import route, run, request, error
 
 # default Landing Page
 
@@ -26,11 +26,48 @@ def user(userid):
 def user(id, name):
     return '<h1> User details of id is :' + id + ' and Name is :' + name + '<h1>'
 
+# route with method
+
 
 @route('/post', method='POST')
 def posted():
     return '<h1>POST Method </h1>'
 
+# query parameter call from url
+# http://localhost:8080/queryparameter?param1=Shubhendu&param2=Abhishek
+
+
+@route('/queryparameter')
+def queryparameter():
+    param1 = request.query.param1
+    param2 = request.query.param2
+
+    return '<h1>The value of param1 is :' + param1 + 'Param2 is : ' + param2 + '</h1>'
+
+# Returning json object
+
+
+@route('/json')
+def resjon():
+    return {'name': 'Shubhendu', 'mylist': [1, 2, 3, 4, 5]}
+
+# Error Handling
+
+
+@error(404)
+def error404(error):
+    return '<h1>You have 404 invalid url</h1>'
+
+
+@error(405)
+def error405(error):
+    return '<h1>You have 405 invalid url</h1>'
+
+
+@error(500)
+def error500(error):
+    return '<h1>You have 500 invalid url</h1>'
+
 
 if __name__ == '__main__':
-    run(debug=True, reloader=True)
+    run(debug=False, reloader=True)
